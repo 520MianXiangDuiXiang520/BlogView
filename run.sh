c65d9e4568e712aa94fb0cf5cb6aace6e20d0914
520MianXiangDuiXiang520 <15364968962@163.com>
2020-09-28 10:36:28 +0800

    :bug:fix run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 Project_Path=$1
 cd $Project_Path
 git pull
+cd $Project_Path/admin-view
+cnpm install
+cnpm run build
+cd $Project_Path/blog-view
+cnpm install
 cnpm run build
 docker restart nginx
