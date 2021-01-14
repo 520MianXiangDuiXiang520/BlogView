27c910171b558d23af458f222382bab19f218da2
520MianXiangDuiXiang520 <15364968962@163.com>
2021-01-14 21:03:39 +0800

    :alembic:压缩编译后的js文件，提高首屏响应速度

--- admin-view/vue.config.js
+++ admin-view/vue.config.js
@@ -1,4 +1,7 @@
 // vue.config.js
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+const productionGzipExtensions = ['js', 'css']
+const isProduction = process.env.NODE_ENV === 'production'
 module.exports = {
     publicPath: "./",
     outputDir: "dist",
@@ -16,4 +19,27 @@ module.exports = {
     //     port: 8080,
     //     proxy: 'http://192.168.1.3:8080'
     // }
+}
+configureWebpack: {
+    plugins: [
+        // Ignore all locale files of moment.js
+        new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+        new UglifyJsPlugin({
+            uglifyOptions: {
+                compress: {
+                    drop_debugger: true,
+                    drop_console: true
+                }
+            },
+            sourceMap: false,
+            parallel: true
+        }),
+        // 配置compression-webpack-plugin压缩
+        new CompressionWebpackPlugin({
+            algorithm: 'gzip',
+            test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+            threshold: 10240,
+            minRatio: 0.8
+        })
+    ]
 }
