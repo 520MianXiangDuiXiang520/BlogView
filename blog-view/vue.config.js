98d198253571ce0e0e7eae551761c8a8ee9f2638
520MianXiangDuiXiang520 <15364968962@163.com>
2021-01-14 23:33:41 +0800

    :zap: 使用 CDN 提高首屏响应速度

--- blog-view/vue.config.js
+++ blog-view/vue.config.js
@@ -1,45 +1,39 @@
-const CompressionWebpackPlugin = require('compression-webpack-plugin')
-const productionGzipExtensions = ['js', 'css']
-const isProduction = process.env.NODE_ENV === 'production'
+const path = require("path");
+
+const webpack = require("webpack");
+const CompressionWebpackPlugin = require("compression-webpack-plugin");
+const productionGzipExtensions = ["js", "css"];
+const isProduction = process.env.NODE_ENV === "production";
 // vue.config.js
 module.exports = {
-    publicPath: "./",
-    outputDir: "dist",
-    assetsDir: "assets",
-    indexPath: "index.html",
-    filenameHashing: true,
-    pages: undefined,
-    lintOnSave: true,
-    runtimeCompiler: false,
-    transpileDependencies: [],
-    productionSourceMap: false,
-    crossorigin: undefined,
-    integrity: false,
-    // devServer: {//代理
-    //     port: 8889,
-    //     // proxy: 'http://192.168.1.3:8080'
-    // }
-}
-configureWebpack: {
-    plugins: [
-        // Ignore all locale files of moment.js
-        new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-        new UglifyJsPlugin({
-            uglifyOptions: {
-                compress: {
-                    drop_debugger: true,
-                    drop_console: true
-                }
-            },
-            sourceMap: false,
-            parallel: true
-        }),
-        // 配置compression-webpack-plugin压缩
-        new CompressionWebpackPlugin({
-            algorithm: 'gzip',
-            test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
-            threshold: 10240,
-            minRatio: 0.8
-        })
-    ]
-}
+  publicPath: "./",
+  outputDir: "dist",
+  assetsDir: "assets",
+  indexPath: "index.html",
+  filenameHashing: true,
+  pages: undefined,
+  lintOnSave: true,
+  runtimeCompiler: false,
+  transpileDependencies: [],
+  productionSourceMap: false,
+  crossorigin: undefined,
+  integrity: false,
+  // devServer: {//代理
+  //     port: 8889,
+  //     // proxy: 'http://192.168.1.3:8080'
+  // }
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+        "@i": path.resolve(__dirname, "./src/assets"),
+      },
+    },
+    externals: {
+      vue: "Vue",
+      "element-ui": "ELEMENT",
+      "bootstrap-vue": "bootstrapVue",
+      "mavon-editor": "MavonEditor",
+    },
+  },
+};
